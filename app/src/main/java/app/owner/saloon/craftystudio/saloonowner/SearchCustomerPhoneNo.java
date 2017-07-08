0b7e0aa7cf8aca58a21720c1bc968e5933c03fa9
Aisha <priyank20patel@gmail.com>
2017-07-09 00:03:21 +0530

    designs of profile,add service,service display done Navigation

---
+++ app/src/main/java/app/owner/saloon/craftystudio/saloonowner/SearchCustomerPhoneNo.java
@@ -0,0 +1,50 @@
+package app.owner.saloon.craftystudio.saloonowner;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import utils.Order;
+import utils.OrderAdapter;
+
+public class SearchCustomerPhoneNo extends AppCompatActivity {
+
+    ArrayList<Order> orderArrayList = new ArrayList<>();
+    OrderAdapter orderAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_customer_phone_no);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.searchby_orderList_recyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+        recyclerView.setAdapter(orderAdapter);
+
+    }
+
+}
